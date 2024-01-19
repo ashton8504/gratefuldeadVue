06f73a13f01fd72a945a077632dc623980e1c8c1
ashton8504 <ashtonlittle8504@gmail.com>
2024-01-19 14:31:00 -0500

    removed nodemailer

--- dead-website-server/server.js
+++ dead-website-server/server.js
@@ -2,19 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
-const nodemailer = require("nodemailer");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const SETLIST_API_KEY = process.env.SETLIST_API_KEY;
 const artistMbid = "6faa7ca7-0d99-4a5e-bfa6-1fd5037520c6";
-const EMAIL = process.env.EMAIL;
-const PASSWORD = process.env.PASSWORD;
 
 app.use(express.json());
 app.use(cors({ origin: "*" }));
-
-console.log(EMAIL, PASSWORD);
 
 app.get("/api/setlist", async (req, res) => {
   try {
@@ -40,41 +35,6 @@ app.get("/api/setlist", async (req, res) => {
     );
     res.status(500).send("Error fetching data");
   }
-});
-
-app.post("/contact", (req, res) => {
-  const { email, message } = req.body;
-  // Basic validation
-  if (!email || !message) {
-    return res
-      .status(400)
-      .json({ error: "Email and message are required fields" });
-  }
-
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.PASSWORD,
-    },
-  });
-
-  const mailOptions = {
-    from: process.env.EMAIL,
-    to: "ashtoncodes8504@gmail.com",
-    subject: "New Message Received",
-    text: `Email: ${email}\nMessage: ${message}`,
-  };
-
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.error("Error sending email:", error);
-      return res.status(500).json({ error: "Error sending email" });
-    } else {
-      console.log("Email sent:", info.response);
-      return res.status(200).json({ message: "Message received!" });
-    }
-  });
 });
 
 app.listen(PORT, () => {
